375ac33bf01484bba587a593770b0a1ebd91b44a
Franco Correa <franco@francocorrea.com>
2024-02-08 23:43:33 -0300

    Implement fake auth for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # TODO: implement proper authentication
+  def current_user
+    User.first
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
 end
